07923df259367da19124f56be04c45e184313963
Oğuzhan Tekelioğlu <130230042+OguzhanTekeli06@users.noreply.github.com>
2024-10-03 22:56:32 +0300

    added AdminBlog

---
+++ AdminBlog/Models/Blog.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Adminblog.Models{
+
+    public class Blog{
+        public int Id { get;set;}
+
+        public string Title {get; set;}
+
+        public string Subtitle { get;set;}
+
+        public string Content {get;set;}
+
+        public string Imagepath {get;set;}
+
+        public string IsPublih {get;set;}
+
+        
+
+        public Author Author {get;set;}
+
+        public int AuthorId {get;set;}
+
+        public Category Category {get;set;}
+    
+
+        public int CategoryId {get;set;}
+    }
+
+
+
+}
