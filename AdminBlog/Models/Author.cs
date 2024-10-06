ac4b7ca08f98b9b7f175328d53d9b82d4cf80aef
Oğuzhan Tekelioğlu <130230042+OguzhanTekeli06@users.noreply.github.com>
2024-10-06 15:47:56 +0300

    Database Operations

--- AdminBlog/Models/Author.cs
+++ AdminBlog/Models/Author.cs
@@ -5,13 +5,13 @@ namespace Adminblog.Models{
     public class Author{
         public int Id { get;set;}
 
-        public string Name {get; set;}
+        public string? Name {get; set;}
 
-        public string Surname { get;set;}
+        public string? Surname { get;set;}
 
-        public string Email {get;set;}
+        public string? Email {get;set;}
 
-        public string Password {get;set;}
+        public string? Password {get;set;}
 
     }
 
--- AdminBlog/Models/Category.cs
+++ AdminBlog/Models/Category.cs
@@ -5,7 +5,7 @@ namespace Adminblog.Models{
     public class Category{
         public int Id { get;set;}
 
-        public string Name {get; set;}
+        public string? Name {get; set;}
     }
 
 
